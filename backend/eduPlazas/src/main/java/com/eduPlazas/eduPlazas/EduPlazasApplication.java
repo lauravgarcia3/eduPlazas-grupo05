81c340d6a5def9ded66266bc60e0d00f3e8d5a0a
Laura Vicente Garcia <laura.vgarcia@alumnos.upm.es>
2026-03-28 13:06:36 +0100

    feat: add initial Spring Boot project

---
+++ backend/eduPlazas/src/main/java/com/eduPlazas/eduPlazas/EduPlazasApplication.java
@@ -0,0 +1,13 @@
+package com.eduPlazas.eduPlazas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduPlazasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduPlazasApplication.class, args);
+	}
+
+}
---
+++ backend/eduPlazas/src/test/java/com/eduPlazas/eduPlazas/EduPlazasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduPlazas.eduPlazas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduPlazasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
